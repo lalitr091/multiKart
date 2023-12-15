b286ee33cc8b012f19a2fd030744f6843ec77ed3
swagata dutta <swagatadutta424@gmail.com>
2023-12-15 13:35:54 +0530

    added razorpay payment integration microservice.

---
+++ razorpay/src/main/java/com/example/razorpay/RazorpayApplication.java
@@ -0,0 +1,15 @@
+package com.example.razorpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.json.JSONObject;
+import com.razorpay.*;
+
+@SpringBootApplication
+public class RazorpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazorpayApplication.class, args);
+	}
+
+}
