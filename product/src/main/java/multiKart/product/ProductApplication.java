d09bc568a04de3845c6acadb63de542ad4232f18
swagata dutta <swagatadutta424@gmail.com>
2023-12-16 13:32:26 +0530

    added product authentication ms to eureka service registry

--- product/src/main/java/multiKart/product/ProductApplication.java
+++ product/src/main/java/multiKart/product/ProductApplication.java
@@ -1,8 +1,10 @@
 package multiKart.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductApplication
 {
 
