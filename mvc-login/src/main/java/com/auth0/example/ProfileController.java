411aab9046f93bf5ab285761566ef7e2421b0200
swagata dutta <swagatadutta424@gmail.com>
2023-12-18 10:11:47 +0530

    modified profile api in mvc-login ms

--- mvc-login/src/main/java/com/auth0/example/ProfileController.java
+++ mvc-login/src/main/java/com/auth0/example/ProfileController.java
@@ -3,12 +3,13 @@ package com.auth0.example;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import common.Constants;
+import model.ApplicationResponse;
 import net.minidev.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,22 +34,34 @@ public class ProfileController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
     private final static ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
-    @Operation(summary = "user profile")
-    @Tag(name = "user profile")
     @GetMapping("/profile")
-    public String profile(Model model, @AuthenticationPrincipal OidcUser oidcUser) {
-        model.addAttribute("profile", oidcUser.getClaims());
-        model.addAttribute("profileJson", claimsToJson(oidcUser.getClaims()));
+    public ApplicationResponse profile(Model model, @AuthenticationPrincipal OidcUser oidcUser) {
+        //model.addAttribute("profile", oidcUser.getClaims());
+        //model.addAttribute("profileJson", claimsToJson(oidcUser.getClaims()));
         // Access the user ID (sub field)
-        String profileJson = oidcUser.getSubject();
-        // Access the user ID (sub field)
-        String userId = extractUserId(profileJson);
-        // Display the user ID
-        //System.out.println("User ID: " + userId);
+        ApplicationResponse applicationResponse = new ApplicationResponse<>();
+        try {
+            String profileJson = oidcUser.getSubject();
+            OidcUserInfo userInfo = oidcUser.getUserInfo();
+            // Access the user ID (sub field)
+            String userId = extractUserId(profileJson);
+            // Display the user ID
+            //System.out.println("User ID: " + userId);
 
-        model.addAttribute("userId",userId);
+            //model.addAttribute("userId",userId);
 
-        return "profile";
+            List list = new ArrayList();
+            list.add(userInfo);
+            applicationResponse.setData(list);
+            applicationResponse.setMessage("user profile");
+            applicationResponse.setStatus(Constants.OK);
+
+        }catch (Exception e){
+            applicationResponse.setStatus(Constants.NOT_FOUND);
+            applicationResponse.setMessage("user profile not found");
+        }
+        return applicationResponse;
+
     }
 
     private static String extractUserId(String sub) {
