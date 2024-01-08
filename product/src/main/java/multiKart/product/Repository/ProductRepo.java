52c9579879e4a1af7528e34aeb873f5b0313c91e
ShwetaTanwar <Shweta.Tanwar@sandata.com>
2024-01-08 18:59:23 +0530

    filter update

--- product/src/main/java/multiKart/product/Repository/ProductRepo.java
+++ product/src/main/java/multiKart/product/Repository/ProductRepo.java
@@ -2,8 +2,11 @@ package multiKart.product.Repository;
 import multiKart.product.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProductRepo extends MongoRepository<Product, String>
 {
@@ -18,5 +21,22 @@ public interface ProductRepo extends MongoRepository<Product, String>
             List<Product> findByTitleContaining(String keyword);
 
 
+    @Query("{'$or': [" +
+            "{'category': { '$in': [ ?0 ] }}," +
+            "{'brand': { '$in': [ ?1 ] }}," +
+            "{'variants': { '$elemMatch': { 'color': { '$in': [ ?2 ] }, 'size': { '$in': [ ?3 ] } } }}," +
+            "]}")
+    List<Product> findByCategoryOrBrandOrVariantsColorOrVariantsSize(
+            String category,
+            List<String> brands,
+            List<String> colors,
+            List<String> sizes,
+            Double minPrice,
+            Double maxPrice
+    );
 
 }
+
+
+
+
